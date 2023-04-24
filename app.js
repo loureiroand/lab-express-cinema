4db913a88ad7a3829cfb72d936022ca14a013962
loureiroand <124377344+loureiroand@users.noreply.github.com>
2023-04-24 11:25:40 +0100

    done

--- app.js
+++ app.js
@@ -20,13 +20,19 @@ require('./config')(app);
 
 // default value for title local
 const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const capitalized = string =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+app.get('/movies', async (req, res) => {
+  const movies = await movies.find();
+  res.render(movies);
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
